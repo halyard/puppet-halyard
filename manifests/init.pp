a4d8405b4541956445d21a7102c4b0d6d490a757
Les Aker <me@lesaker.org>
2017-01-16 22:19:18 -0500

    add secure_path

--- manifests/init.pp
+++ manifests/init.pp
@@ -34,6 +34,7 @@ class halyard (
     require_password      => false,
     comment               => 'Allows halyard user to run puppet',
     require_exist         => false,
-    allowed_env_variables => ['DEBUG', 'PUPPET_ENV']
+    allowed_env_variables => ['DEBUG', 'PUPPET_ENV'],
+    defaults              => ['secure_path = /sbin:/bin:/usr/sbin:/usr/bin']
   }
 }

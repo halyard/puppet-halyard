25f5073638cd13d6fbcc089ad073d5296dc1dc28
Les Aker <me@lesaker.org>
2017-04-29 15:58:32 -0400

    update fact

--- lib/facter/github_token.rb
+++ lib/facter/github_token.rb
@@ -3,8 +3,7 @@ require 'yaml'
 Facter.add('github_token') do
   has_weight 10
   setcode do
-    user = Facter.value(:id)
-    github_token_file = "/Users/#{user}/.octoauth.d/halyard.yml"
+    github_token_file = "/var/root/.octoauth.d/halyard.yml"
     return nil unless File.exist? github_token_file
     YAML.load(File.read(github_token_file))['halyard']
   end

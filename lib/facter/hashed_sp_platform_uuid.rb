776cd0491aa49873bb85b967264bbed61bb7f6d7
Les Aker <me@lesaker.org>
2018-05-23 20:08:40 -0400

    fix typo

--- lib/facter/hashed_sp_platform_uuid.rb
+++ lib/facter/hashed_sp_platform_uuid.rb
@@ -1,6 +1,6 @@
 Facter.add('hashed_sp_platform_uuid') do
     has_weight 10
     setcode do
-      Digest::SHA512.hexdigest(Facter.id(:sp_platform_uuid))[0...24]
+      Digest::SHA512.hexdigest(Facter.value(:sp_platform_uuid))[0...24]
     end
 end
